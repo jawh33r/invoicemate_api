08c4f291fef97c05e7827837c30014e616b66e8e
jawh3r <jawh3r@gmail.com>
2025-02-09 14:35:58 +0100

    hata chy

--- config/database.js
+++ config/database.js
@@ -3,10 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.BD_CONNECT, {
     dialect: 'postgres',
-    logging: false
+    logging: false,
 });
 
 module.exports = sequelize;
